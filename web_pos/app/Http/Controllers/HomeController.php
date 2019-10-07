994b20726a3610ee05aec2a309d301ed5113781d
rizalkhisyam <162410101048@cs.unej.ac.id>
2019-10-07 23:27:54 +0700

    commit pertama

---
+++ web_pos/app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Auth;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function dashboard(){
+        if(Auth::User()->level=='1'){
+            return view('admin.index');
+        }else{
+            return view('welcome');
+        }
+    }
+}
---
+++ web_pos/database/migrations/2019_10_07_125337_barang.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Barang extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_barang');
+            $table->char('kode', 5)->unique();
+            $table->integer('harga');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
