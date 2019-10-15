f7ce0e2277189b1c8645bf9e1fc8fea71f1c6a8a
faisolrisqi <faisolakbar0@gmail.com>
2019-10-15 22:40:04 +0700

    Update Barang

--- web_pos/routes/web.php
+++ web_pos/routes/web.php
@@ -20,5 +20,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@dashboard');
 Route::get('/admins','AdminController@index');
 Route::get('/tambahData','AdminController@tambah');
-Route::get('/tambahDataBarang','BarangController@tambahDataBarang');
+Route::get('/tambahDataBarang','BarangController@dataBarang');
 Route::get('/dataTransaksi','TransaksiController@lihatDataTransaksi');
+
+/* Bagian Barang */
+Route::post('/tambahBarang','BarangController@tambahBarang');
